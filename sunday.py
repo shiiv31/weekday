d3df14fe132d3609b62eb3c7dc08628beceeec9a
shivanipurii31@gmail.com <shivaniipuri31@gmail.com>
2021-11-01 20:46:54 +0545

    OOP

--- sunday.py
+++ sunday.py
@@ -1,1 +1,7 @@
-'hi hi hi hi '
+class Student:
+    def __init__(self, student_f_name , student_l_name):
+        self.first_name = student_f_name
+        self.last_name = student_l_name
+
+s =  Student ('shivani' ,'puri')
+print (s.first_name , s.last_name)
