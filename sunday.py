b73a78e421907e6305f6066662d5ec67f8ccb043
shivanipurii31@gmail.com <shivaniipuri31@gmail.com>
2021-11-01 21:08:07 +0545

    Variable

--- sunday.py
+++ sunday.py
@@ -1,7 +1,26 @@
 class Student:
+    school_name= 'D.A.V.S.K.V.B'
+    Principal_name= 'Mrs. Bhuvaneshwori Rao'
     def __init__(self, student_f_name , student_l_name):
         self.first_name = student_f_name
         self.last_name = student_l_name
 
+        def get_name(self):
+            return self.first_name
+
+
+    @classmethod
+    def get_school_name(cls):
+            return cls.school_name
+
+    @staticmethod
+    def add (a,b):
+            print (a+b)
+
+
+
+
 s =  Student ('shivani' ,'puri')
-print (s.first_name , s.last_name)
+print (Student.get_school_name())
+
+Student.add(2,3)
